30ddd5b51463d1f9ae781417ae40b9c7fac6e303
Robert Haskell <rjhaskell23@gmail.com>
2017-03-13 22:34:51 -0700

    changed a few things

--- src/js/app.controller.ts
+++ src/js/app.controller.ts
@@ -7,23 +7,24 @@ namespace Lab06 {
         public operator: string = ""
         public result: number = 0;
 
-        /**             //popped out with calculate method!
-         * calculate
-         */
-         public calculate(num1, num2, operator, result) {
-            if(operator == "ADD") {      //Addition calculation
-                let result = num1 + num2;
-                return result;
-            }else if(operator == "SUB") {//Subtraction calculation
-                let result: number = num1 - num2;
-                return result;
-            }else if(operator == "MUL") {//Multiplication calculation
-                let result: number = num1 * num2;
-                return result;
-            }else if(operator == "DIV") {//Division calculation
-                let result: number = num1 / num2;
-                return result;
-            }
+        
+         public calculate(num1: number, num2: number, operator: string, result: number): number{
+            if(operator === "ADD") {      //Addition calculation
+                return num1 + num2;
+              
+            };
+            if(operator === "SUB") {//Subtraction calculation
+                return num1 - num2;
+            
+            };
+            if(operator === "MUL") {//Multiplication calculation
+                return num1 * num2;
+        
+            };
+            if(operator === "DIV") {//Division calculation
+              return num1 / num2;
+            
+            };
         }
 
     }
